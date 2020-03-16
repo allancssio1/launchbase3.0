c2a5ab3b14fd40c9f5140c9cc4f94aae9e456fc0
Allan Freitas <allan.cassio1@gmail.com>
2020-03-16 03:39:40 -0300

    ainda nao comcluido o desafio do iframe, desafio 2-3

--- iniciando-Front-End/exercicioFavicon/script.js
+++ iniciando-Front-End/exercicioFavicon/script.js
@@ -1,5 +1,7 @@
 const modalOverlay = document.querySelector('.modalOverlay')
 const cards = document.querySelectorAll('.card')
+const maximModal = document.querySelector('.maxim-modal')
+const modal = document.querySelector(".modal")
 
 for( let card of cards){
     card.addEventListener("click", function(){
@@ -7,6 +9,18 @@ for( let card of cards){
     })
 }
 
-document.querySelector('close-modal').addEventListener("click", functio(){
-    modalOverlay.classList.remove('active')
+document.querySelector(".close-modal").addEventListener("click", function(){
+    modalOverlay.classList.remove("active")
+})
+
+maximModal.addEventListener("click", function(){
+
+    if(maximModal.contains("up") == false){
+        modal.classList.add("up")
+        maximModal.classList.add("up")
+    }else if(maximModal.contains(".up") == true){
+        modal.classList.remove("up")
+        maximModal.classList.remove("up")
+    }
+
 })
