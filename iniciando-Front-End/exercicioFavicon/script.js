bf85207c36a31d9a29e922af018e8686d5816ffa
Allan Freitas <allan.cassio1@gmail.com>
2020-03-16 20:15:12 -0300

    concluido o exercicio iniciando-Front-end e exercicioFavicon

--- iniciando-Front-End/exercicioFavicon/script.js
+++ iniciando-Front-End/exercicioFavicon/script.js
@@ -5,22 +5,26 @@ const modal = document.querySelector(".modal")
 
 for( let card of cards){
     card.addEventListener("click", function(){
+        const idIframe = card.getAttribute("id")
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${idIframe}`
     })
 }
 
 document.querySelector(".close-modal").addEventListener("click", function(){
     modalOverlay.classList.remove("active")
+    modal.classList.remove('up')
+    maximModal.classList.remove('up')
+    modalOverlay.querySelector("iframe").src = ""
 })
 
 maximModal.addEventListener("click", function(){
 
-    if(maximModal.contains("up") == false){
-        modal.classList.add("up")
-        maximModal.classList.add("up")
-    }else if(maximModal.contains(".up") == true){
-        modal.classList.remove("up")
-        maximModal.classList.remove("up")
+    if(modal.classList.contains("up") === false){
+        modal.classList.add('up')
+        maximModal.classList.add('up')
+    }else if(modal.classList.contains('up') === true){
+        modal.classList.remove('up')
+        maximModal.classList.remove('up')    
     }
-
 })
