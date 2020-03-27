92dcfc32cae789c7d593324e42d522e6cf048437
Allan Freitas <allan.cassio1@gmail.com>
2020-03-27 01:03:23 -0300

    dando contuidade do back-end

--- iniciando-Back-End/server.js
+++ iniciando-Back-End/server.js
@@ -3,13 +3,16 @@ const nunjucks = require('nunjucks')
 
 const server = express()
 
+server.use(express.static('public'))
+
 server.set("view engine", "html")
 
-nunjucks.configure("views" {
+nunjucks.configure("views", {
     express:server
 })
+
 server.get("/", function(require, response){
-    return response.send('restart')
+    return response.render("index")
 })
 
 server.listen(5000, function(){
