c91a021699a1d31c143d4e18e3f4d06c654b9747
Allan Freitas <allan.cassio1@gmail.com>
2020-03-31 18:16:00 -0300

    criado data.js, gerando array no server.js inportando do data.js e exportando de portifolio para data.js

---
+++ iniciando-Back-End/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Fomrulário anmado com js e css com Mayk Brito.",
+        duration: "57min",
+        price: "Free"
+    },
+    {
+        id: "ghTrp1x_1As",
+        title: "O que é API, Rest e Restfull.",
+        duration: "33min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando CSS Grid na prática.",
+        duration: "36min",
+        price: "Free"
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        title: "Desvendando a variável THIS",
+        duration: "48min",
+        price: "Free"
+    },
+    {
+        id: "rAzHvYnQ8DY",
+        title: "Array: High order functions",
+        duration: "54min",
+        price: "Free"
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "GIT e GitHub na prática",
+        duration: "33min",
+        price: "Free"
+    },
+]
