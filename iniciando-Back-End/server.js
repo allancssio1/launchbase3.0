acb474c39076548937d5f526fa9a69a612b61873
Allan Freitas <allan.cassio1@gmail.com>
2020-04-03 19:39:25 -0300

    ajustado grid no style.css, dado continuidade ao servidor dinâmico com nunjucks

--- iniciando-Back-End/server.js
+++ iniciando-Back-End/server.js
@@ -9,11 +9,24 @@ server.use(express.static('public'))
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    autoescape: false
 })
 
 server.get("/about", function(require, response){
-    return response.render("about")
+    const about = {
+        avatarUrl: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        name: "Mayk Brito",
+        role: "FullStack Developer",
+        description: 'Instrutor na <a href="https://rocketseat.com.br/" target="_blanck">&copy;Rocketseat</a> e colaborador na comunidade JavaScript.',
+        links: [
+            {name: "GitHub", url: "https://github.com/maykbrito"},
+            {name: "Linkedin", url: "https://www.linkedin.com/in/maykbrito/"},
+            {name: "Twitter", url: "https://twitter.com/maykbrito"}
+        ]
+    }
+
+    return response.render("about", {about})
 })
 server.get("/portifolio", function(require, response){
     return response.render("portifolio", {itens: videos})
