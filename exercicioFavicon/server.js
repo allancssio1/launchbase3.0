ba4f878b4cc6019a8a25e6c5201d1f1493413d99
Allan Freitas <allan.cassio1@gmail.com>
2020-04-09 03:42:08 -0300

    aditado arquivo conteudo.njk e server.js

--- exercicioFavicon/server.js
+++ exercicioFavicon/server.js
@@ -12,9 +12,29 @@ nunjucks.configure("views", {
     express: server,
     autoescape: false
 })
-
+// server.get("/layout", function(request,response){
+//     layout = {
+//         headers: [
+//             {name: "Home", target: "_self", url: "/index"},
+//             {name: "Conteúdo", target: "_self", url: "/conteudo"},
+//             {name: "Comunidade", target: "_blank", url: "https://discordapp.com/invite/gCRAFhc"},
+//             {name: "E-mail", target: "_blank", url: "oi@rocketseat.com.br"},
+//             {name: "Telefone", target: "_blank", url: "+5547992078767"},
+//             {name: "Sobre", target: "_blank", url: "https://rocketseat.com.br/"}
+//         ]
+//     }
+//     return response("layout", {layout})
+// })
 server.get("/index", function(request, response){
     const index = {
+        headers: [
+            {name: "Home", target: "_self", url: "/index"},
+            {name: "Conteúdo", target: "_self", url: "/conteudo"},
+            {name: "Comunidade", target: "_blank", url: "https://discordapp.com/invite/gCRAFhc"},
+            {name: "E-mail", target: "_blank", url: "oi@rocketseat.com.br"},
+            {name: "Telefone", target: "_blank", url: "+5547992078767"},
+            {name: "Sobre", target: "_blank", url: "https://rocketseat.com.br/"}
+        ],
         footers: [
             {name: "GitHub", url: "https://github.com/Rocketseat"},
             {name: "Instagram", url: "https://www.instagram.com/rocketseat_oficial/"},
@@ -24,22 +44,21 @@ server.get("/index", function(request, response){
     return response.render("index", {index})
 })
 
-server.get("/layout", function(request, response){
-    const layout = { 
+server.get("/conteudo", function(request, response){
+    content = {
         headers: [
-        {name: "Home", target:"_self", url: "/index"},
-        {name: "Conteúdo", target:"_self", url: "/conteudo"},
-        {name: "Comunidade", target:"_blank", url: "https://discordapp.com/invite/gCRAFhc"},
-        {name: "E-mail", target:"_blank", url: "oi@rocketseat.com.br"},
-        {name: "Telefone", target:"_blank", url: "+5547992078767"},
-        {name: "Sobre", target:"_blank", url: "https://rocketseat.com.br/"}
+            {name: "Home", target: "_self", url: "/index"},
+            {name: "Conteúdo", target: "_self", url: "/conteudo"},
+            {name: "Comunidade", target: "_blank", url: "https://discordapp.com/invite/gCRAFhc"},
+            {name: "E-mail", target: "_blank", url: "oi@rocketseat.com.br"},
+            {name: "Telefone", target: "_blank", url: "+5547992078767"},
+            {name: "Sobre", target: "_blank", url: "https://rocketseat.com.br/"}
+        ],
+        cards: [
+            {}
         ]
     }
-    return response.render("layout", {layout})
-})
-
-server.get("/conteudo", function(request, response){
-    return response.render("conteudo")
+    return response.render("conteudo", {content})
 })
 
 server.listen(5000, function(){
